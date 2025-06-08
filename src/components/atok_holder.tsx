2c41dbbb1d11d27a61b144db5416aec0a4c205cc
kelvin ngene <162456111+kelvin227@users.noreply.github.com>
2025-06-08 08:56:46 +0100

    set handledelete to use fromData.adId

--- src/components/atok_holder.tsx
+++ src/components/atok_holder.tsx
@@ -206,7 +206,7 @@ const response = await addtraderequest(
       ...prev,
       adId: ad.id,
     }));
-    const del = await deletead(ad.Id);
+    const del = await deletead(formData.adId);
     if(!del.success){
       toast.error("unable to delete ad");
     }else{
